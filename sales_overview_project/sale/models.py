d1ac78c7bf905ae0b464ad0b3382a50a91de7ee3
unknown <AShukelovich@gmail.com>
2023-11-04 17:49:25 +0300

    added models

--- sales_overview_project/sale/models.py
+++ sales_overview_project/sale/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Delivery(models.Model):
+    delivery_type = models.CharField(max_length=150)
+
+
+class Payment(models.Model):
+    payment_type = models.CharField(max_length=150)
+
+
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=150)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=150)
+    passport_number = models.CharField(max_length=150)
+    birthdate = models.DateTimeField(default=datetime.datetime.now())
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    cost = models.FloatField
+    cost_price = models.FloatField
+
+
+class Sale(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, related_name='deliveries')
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name='payments')
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='shops')
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='employees')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+    product = models.ForeignKey
